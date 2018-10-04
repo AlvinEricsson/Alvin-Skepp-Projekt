fc4da2092d0d95fbd2e7df924039ad6f37fbf352
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2018-10-04 09:26:43 +0200

    Alvin - Unity Project

---
+++ Assets/MyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    public Color color;
+    public float movespeed = 5f;
+    public float rotationspeed = 360f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
